46e9e2ccd4304a5ecc7d4af6c31240d27ea3f815
zakizaki-ri9 <kic.yuuki@gmail.com>
2018-08-12 11:21:43 +0900

    add hello world

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+
+    # CRLFに引っかかった際の対処 => :exception = 例外を創出
+    protect_from_forgery with: :exception
+
+    def hello
+        render html: "hello, world!"
+    end
+
 end
